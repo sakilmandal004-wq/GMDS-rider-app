09a004ddcdcaaa01891ae127d3035e3dfd27be83
sakilmandal004-wq <sakil.mandal004@gmail.com>
2026-09-18 10:22:57 +0530

    Enhance service worker fetch handling with caching

--- sw.js
+++ sw.js
@@ -1,4 +1,37 @@
+const CACHE_NAME = 'gmds-rider-v1';
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  './logo-192.png',
+  './logo-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
-    // Basic Service Worker Fetch Bypass
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
